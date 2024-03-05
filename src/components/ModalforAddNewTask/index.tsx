cd85048b609791f655c4c60812652201a47516bf
Samuel Vitor <samuelvitor668@gmail.com>
2024-03-05 13:21:27 -0300

    fix: fix function create task in Modal component

--- src/components/ModalforAddNewTask/index.tsx
+++ src/components/ModalforAddNewTask/index.tsx
@@ -8,20 +8,24 @@ import {
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import axios from "axios";
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { useAlert } from "react-alert";
 
 interface ModalforAddNewTaskProps {
   children: ReactNode,
   getTasksApi: () => void;
 }
 
-function ModalforAddNewTask({ children,  getTasksApi}: ModalforAddNewTaskProps) {
+function ModalforAddNewTask({ children, getTasksApi }: ModalforAddNewTaskProps) {
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true)
+  const alert = useAlert()
 
   const [nameTask, setNameTask] = useState<string>("")
 
   async function handleCrateNewTask(event) {
     if (nameTask.length === 0) {
-      return alert("Insira nome da tarefa")
+      return
     }
     event.preventDefault();
 
@@ -30,6 +34,8 @@ function ModalforAddNewTask({ children,  getTasksApi}: ModalforAddNewTaskProps) 
         description: nameTask,
         isCompleted: false
       });
+      setIsModalOpen(false)
+      alert.success("Tarefa adicionada!");
       getTasksApi()
     } catch (error) {
       console.error("Erro ao criar a tarefa:", error);
@@ -38,52 +44,59 @@ function ModalforAddNewTask({ children,  getTasksApi}: ModalforAddNewTaskProps) 
     setNameTask("")
   }
 
-  
+  useEffect(() => {
+    if (isModalOpen === false) {
+      setIsModalOpen(true);
+    }
+  }, [isModalOpen])
 
   return (
     <>
-      <Dialog >
-        <DialogTrigger asChild>
-          {children}
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] text-zinc-300 bg-zinc-950">
-          <DialogHeader>
-            <DialogTitle className="text-4xl ">
-              Digite a nova tarefa:
-            </DialogTitle>
-          </DialogHeader>
+      {isModalOpen && (
+        <Dialog >
+          <DialogTrigger asChild>
+            {children}
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px] text-zinc-300 bg-zinc-950">
+            <DialogHeader>
+              <DialogTitle className="text-4xl ">
+                Digite a nova tarefa:
+              </DialogTitle>
+            </DialogHeader>
 
-          <form className="gap-4 py-4" id="meuFormulario">
-            <Input
-              required
-              type="text"
-              value={nameTask}
-              placeholder="Digite a tarefa"
-              className="max-w-xs h-10 text-2xl py-6 "
-              onChange={(e) => setNameTask(e.target.value)}
-            />
-            <DialogFooter className="flex gap-8 mt-10">
-              <button
-                type="submit"
-                className=" border border-zinc-500 px-3 rounded hover:scale-105 transition-all"
-              >
-                Cancelar
-              </button>
+            <form className="gap-4 py-4" id="meuFormulario">
+              <Input
+                required
+                type="text"
+                value={nameTask}
+                placeholder="Digite a tarefa"
+                className="max-w-xs h-10 text-2xl py-6 "
+                onChange={(e) => setNameTask(e.target.value)}
+              />
+              <DialogFooter className="flex gap-8 mt-10">
+                <button
+                  type="submit"
+                  className=" border border-zinc-500 px-3 rounded hover:scale-105 transition-all"
+                >
+                  Cancelar
+                </button>
 
-              <button
-                className="bg-zinc-600 px-3 rounded hover:brightness-75 hover:scale-105 transition-all"
-                type="submit"
-                onClick={(e) => handleCrateNewTask(e)}
-              >
-                Adicionar
-              </button>
-            </DialogFooter>
-          </form>
+                <button
+                  className="bg-zinc-600 px-3 rounded hover:brightness-75 hover:scale-105 transition-all"
+                  type="submit"
+                  onClick={(e) => handleCrateNewTask(e)}
+                >
+                  Adicionar
+                </button>
+              </DialogFooter>
+            </form>
 
-        </DialogContent>
-      </Dialog>
+          </DialogContent>
+        </Dialog>
+      )}
+
     </>
   );
 }
 
-export default ModalforAddNewTask;
+export default ModalforAddNewTask
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.tsx'
+import { App } from './App.tsx'
+import { Provider as AlertProvider, positions, transitions } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 import './index.scss'
 import './index.css'
 
+const options = {
+  // you can also just use 'bottom center'
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: transitions.SCALE,
+};
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
+    </AlertProvider>
   </React.StrictMode>,
 )
